376138c0c2d68a9ec05d2b041fb69d4fb6ca15ce
Etienne CHIAPPINO <chiappinoe@gmail.com>
2023-12-22 15:38:48 +0100

    fix CSS

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                <div class="flex flex-col items-start p-6 space-y-6 text-gray-900 dark:text-gray-100">
+                <div class="flex flex-col p-6 space-y-6 text-gray-900 items-star dark:text-gray-100">
                     <a href="{{ route('posts.create') }}"
                         class="right-0 px-5 py-2 text-sm font-semibold leading-6 text-gray-900 rounded-lg shadow-md hover:bg-zinc-500 hover:text-white focus:bg-zinc-400 focus:shadow-inner hover:scale-200 bg-zinc-200 ">Add
                         Post</a>
