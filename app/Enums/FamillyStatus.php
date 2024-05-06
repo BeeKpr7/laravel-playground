a9c8b0dbec6cf862b31b64086d606d32649a776c
Étienne Chiappino <echiappino@mozartdigital.fr>
2024-05-06 10:00:27 +0200

    Big commit of everything
    Création d'un enuim pour le status familial
    Creation d'un service en cours
    Rien de fini ...

---
+++ app/Enums/FamillyStatus.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Enums;
+
+enum FamillyStatus : int
+{
+
+    case CELIBATAIRE = 1;
+    case MARIE       = 2;
+    case CONCUBINAGE = 3;
+    case VEUF        = 4;
+
+
+    public function label(): string
+    {
+        return match ( $this ) {
+            self::CELIBATAIRE => 'Célibataire',
+            self::MARIE       => 'Marié(e)/Pacsé(e)',
+            self::CONCUBINAGE => 'Concubinage',
+            self::VEUF        => 'Veuf(ve)',
+        };
+    }
+
+    public function is( ServerStatus $value ): bool
+    {
+        return $this->value === $value;
+    }
+
+}
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Enums\ServerStatus;
+use App\Service\Simulateur;
 
 class TestController extends Controller
 {
@@ -16,7 +17,20 @@ class TestController extends Controller
            ServerStatus::MAINTENANCE, //Instance of ServerStatus enum
            ServerStatus::from(1), // Instance of ServerStatus enum with value 1 (ONLINE) return error if not found
            ServerStatus::tryFrom(1)?->label(), // Online
-           ServerStatus::ONLINE->is(ServerStatus::from(3)), // false
+           ServerStatus::ONLINE->is(ServerStatus::from(3)),
+           ServerStatus::ONLINE === ServerStatus::ONLINE ,// true
+           ServerStatus::OFFLINE
         );
+    }
+
+    public function objet()
+    {
+        // echo "test";
+        //echo new Simulateur();
+    }
+
+    public function eric()
+    {
+        return view('eric');
     }
 }
